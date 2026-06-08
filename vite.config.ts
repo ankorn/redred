d3a722e9003652e9e76611970da8442b58b53b58
ant.korneev <ant.korneev@tinkoff.ru>
2026-06-08 11:21:08 +0400

    frontend; vite defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/redred/",
+  base: "/redred/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/hf": {
-        target: "https://huggingface.co",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/hf/, ""),
-      },
-    },
-  },
 });
