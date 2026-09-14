e4a077d30a2391b0c49f26242350d957cf81829c
ant.korneev <ant.korneev@tinkoff.ru>
2026-09-14 17:23:25 +0400

    removed mirror usage

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-import { env } from "@huggingface/transformers";
+// import { env } from "@huggingface/transformers";
 
-env.remoteHost = "https://functions.yandexcloud.net/d4eg9ap7ucsan36isgk8";
+// env.remoteHost = "https://functions.yandexcloud.net/d4eg9ap7ucsan36isgk8";
 
-env.remotePathTemplate =
-  "?url=https://modelscope.cn/models/{model}/resolve/main/";
+// env.remotePathTemplate =
+//   "?url=https://modelscope.cn/models/{model}/resolve/main/";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
